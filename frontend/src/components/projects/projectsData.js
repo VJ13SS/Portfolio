16c205bd53cfdd0f5e4626c4df05662c73ce1ac4
Viswajith S <135973734+VJ13SS@users.noreply.github.com>
2025-03-23 20:29:03 +0530

    Update projectsData.js
    Updated the project descriptions

--- frontend/src/components/projects/projectsData.js
+++ frontend/src/components/projects/projectsData.js
@@ -13,9 +13,9 @@ const projectsInfo = [
     image: "/project1 (1).png",
   },
   {
-    name: "OFCA - Online Fitness Coach Academy",
+    name: "NOTFLIX ",
     description:
-      "A client project i got through linkedin. The project focusses on delivering his fitness plas to the users,..Also shares ideas about he plans being offered,the cost,informstions about the program",
+      "A React and Flask app that provides personalized movie recommendations using item-based collaborative filtering on the MovieLens dataset, with authentication, dynamic updates, and search.",
     techStack: "React,Express Js,Node.js,Email js(for emails),stripe(payment gateway)",
     links: [
       
@@ -26,9 +26,9 @@ const projectsInfo = [
     image: "/project2.jpg",
   },
   {
-    name: "OFCA - Online Fitness Coach Academy",
+    name: "Py-Blog",
     description:
-      "A client project i got through linkedin. The project focusses on delivering his fitness plas to the users,..Also shares ideas about he plans being offered,the cost,informstions about the program",
+      "A Flask-based blog app for creating, editing, deleting posts, liking, commenting, and managing profiles. Built to reinforce Flask concepts, with room for further development.",
     techStack: "React,Express Js,Node.js,Email js(for emails),stripe(payment gateway)",
     links: [
       
@@ -39,9 +39,9 @@ const projectsInfo = [
     image: "/project3.jpg",
   },
   {
-    name: "OFCA - Online Fitness Coach Academy",
+    name: "Bank Customer Churn Prediction",
     description:
-      "A client project i got through linkedin. The project focusses on delivering his fitness plas to the users,..Also shares ideas about he plans being offered,the cost,informstions about the program",
+      "Built a machine learning model using data preprocessing, SMOTE, and model evaluation, identifying Random Forest as the best performer for real-time predictions.",
     techStack: "React,Express Js,Node.js,Email js(for emails),stripe(payment gateway)",
     links: [
       
